44ecaeb2ce3be36dc8d60d0bed9c25dbd4c204af
Seshagiri Prabhu <seshagiri.narasimha1@louisiana.edu>
2022-11-20 22:20:34 -0600

    Using the model to automatically pick the name and path of the django template for the user signup view

--- users/views.py
+++ users/views.py
@@ -3,18 +3,12 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from users.forms import CustomUserCreationForm
+from users.models import CustomUser
 
 
 class SignUpView(generic.CreateView):
-    """User registration view.
+    """User registration view."""
 
-    Arguments:
-    ---------
-    generic : object
-        Django generic `CreateView`.
-
-    """
-
+    model = CustomUser
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
