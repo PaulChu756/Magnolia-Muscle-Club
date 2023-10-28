e2a14fe38b0f3ba8803caecaab1d8d53a981a9d4
Devansmith1999 <smithdevan1999@gmail.com>
2023-10-28 13:25:24 -0500

    Updated Achievements Model

--- mmc/admin.py
+++ mmc/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PersonalBest, Weight, Workout
+
+
+@admin.register(Weight)
+class WeightAdmin(admin.ModelAdmin):
+    list_display = ["value", "date"]
+    # Add any other customizations here
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ["name", "workout_type"]
+    # Add any other customizations here
+
+
+@admin.register(PersonalBest)
+class PersonalBestAdmin(admin.ModelAdmin):
+    list_display = ["workout", "value"]
+    # Add any other customizations here
+
+
+# @admin.register(Chart)
+# class ChartAdmin(admin.ModelAdmin):
+#     list_display = ["workout", "workouts_per_week"]
+#     # Add any other customizations here
--- mmc/models.py
+++ mmc/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+# Define the Workout model with a dropdown select menu
+class Workout(models.Model):
+    WORKOUT_CHOICES = [
+        ("Cardio", "Cardio"),
+        ("Strength Training", "Strength Training"),
+        ("Yoga", "Yoga"),
+        # Add more workout types as needed
+    ]
+    name = models.CharField(max_length=100)
+    workout_type = models.CharField(max_length=50, choices=WORKOUT_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+# Define the Weight model
+class Weight(models.Model):
+    value = models.PositiveIntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"Weight: {self.value} lbs ({self.date})"
+
+
+# Define the Personal Best model
+class PersonalBest(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"Personal Best for {self.workout}"
