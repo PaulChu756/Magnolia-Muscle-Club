5f84019c2d6d9eca0b499c5b2114627c44e9aff6
colinlanclos <colinlanclos@gmail.com>
2023-10-29 17:31:27 -0500

    I put in the models for FoodLibaray and WorkOoutVideos,Tested them

--- mmc/admin.py
+++ mmc/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import WorkOutVideos, FoodLibrary
+
+admin.site.register(WorkOutVideos)
+admin.site.register(FoodLibrary)
 
 # Register your models here.
--- mmc/models.py
+++ mmc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class WorkOutVideos(models.Model):
+    workOutDay = models.CharField(max_length=200)
+    workOutVideoLink = models.CharField(max_length=200)
+
+class FoodLibrary(models.Model):
+    foodDay = models.CharField(max_length=200)
+    foodChoice = models.CharField(max_length=200)
+
+
 # Create your models here.
