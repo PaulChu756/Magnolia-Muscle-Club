8a4413a706ba55b50fad0e82af94502bfa017cf5
Devansmith1999 <smithdevan1999@gmail.com>
2023-10-30 12:44:21 -0500

    completed and tested achievements

--- mmc/admin.py
+++ mmc/admin.py
@@ -2,23 +2,27 @@ from django.contrib import admin
 
 from .models import PersonalBest, Weight, Workout
 
-
-@admin.register(Weight)
-class WeightAdmin(admin.ModelAdmin):
-    list_display = ["value", "date"]
-    # Add any other customizations here
+admin.site.register(PersonalBest)
+admin.site.register(Weight)
+admin.site.register(Workout)
 
 
-@admin.register(Workout)
-class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ["name", "workout_type"]
-    # Add any other customizations here
+# @admin.register(Weight)
+# class WeightAdmin(admin.ModelAdmin):
+#     list_display = ["value", "date"]
+#     # Add any other customizations here
 
 
-@admin.register(PersonalBest)
-class PersonalBestAdmin(admin.ModelAdmin):
-    list_display = ["workout", "value"]
-    # Add any other customizations here
+# @admin.register(Workout)
+# class WorkoutAdmin(admin.ModelAdmin):
+#     list_display = ["name", "workout_type"]
+#     # Add any other customizations here
+
+
+# @admin.register(PersonalBest)
+# class PersonalBestAdmin(admin.ModelAdmin):
+#     list_display = ["workout", "value"]
+#     # Add any other customizations here
 
 
 # @admin.register(Chart)
